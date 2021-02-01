8db6801eef6ea39631248a15f480892031c116dc
nimowairimu <nimowairimu25@gmail.com>
2021-02-01 21:59:07 +0300

    make new form to add Quotes.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,12 @@ import { Quote } from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote('go outside its nice and hot','Nimo','Rupi Kaur',0,0, new Date(2020,9,12)),
-   new Quote ('go outside its nice and hot','Nimo','Ainstein',0,0,new Date (2020,9,8)),
-   new Quote ('go outside its nice and hot','Nimo','Nimo',0,0, new Date (2020,8,9)),
-  new Quote('go outside its nice and hot','Nimo','Nims',0,0,new Date (2020,6,7)),
+    new Quote('Tough times never last but tough people do','Susan','Robert H. Schiuller',0,0, new Date(2020,9,12)),
+   new Quote ('Have enough courage to start and enough heart to finish.','Racheal','Jessica N. S. Yourko',0,0,new Date (2020,9,8)),
+   new Quote ('Hate comes from intimidation, love comes from appreciation','Wendy','Tyga',0,0, new Date (2020,8,9)),
+  new Quote('Reality is wrong, dreams are for real.','Mell','Tupac',0,0,new Date (2020,6,7)),
+  new Quote('There is no substitute for hard work.','Rose','Thomas Edison',0,0,new Date (2020,6,7)),
+  new Quote('What consumes your mind controls your life','Leah','Maya Angelou',0,0,new Date (2020,6,7)),
   ];
 
   toggleDetails(index){
